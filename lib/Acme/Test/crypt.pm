e5ec57ed03c466a5e01fe968db1a1a06bf58a918
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-20 19:09:43 +0700

    Initial commit

---
+++ lib/Acme/Test/crypt.pm
@@ -0,0 +1,12 @@
+package Acme::Test::crypt;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Check crypt() support in various platforms
+
+=head1 DESCRIPTION
+
+This distribution is created for testing C<crypt()> support on various
+platforms. See the test suite.
